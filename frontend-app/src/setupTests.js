7f779a3f81c8a60a83e3d8533c1026aaeb288293
Daksh Batra <dakshbatra2007@gmail.com>
2025-10-03 13:58:53 +0530

    Empty repo: remove all tracked files

--- frontend-app/src/setupTests.js
+++
@@ -1,4 +0,0 @@
-// jest-dom adds custom jest matchers for testing DOM nodes. I use it so I can write tests like:
-// expect(element).toHaveTextContent(/react/i)
-// I found out you can learn more here: https://github.com/testing-library/jest-dom. It's pretty useful for testing React stuff.
-import '@testing-library/jest-dom';
